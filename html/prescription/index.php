b3e7020191ae64752ad38ec89848732b9314371b
KyomaHooin <kyomahooin@gmail.com>
2022-08-26 20:55:49 +0200

    Missing year.

--- html/prescription/index.php
+++ html/prescription/index.php
@@ -64,12 +64,13 @@ if (preg_match('/drop:.*/', $raw)) {
 		echo '<div class="alert alert-warning alert-dismissible fade show" role="alert">Žádná data.<button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button></div>';
 	} else {
 		echo '<table class="table">';
-		echo '<thead class=""><tr><th scope="col">Datum</th><th scope="col">Jméno</th><th scope="col">Recept</th></tr>';
+		echo '<thead class=""><tr><th scope="col">Datum</th><th scope="col">Jméno</th scope="col"><th>Rok</th><th scope="col">Recept</th></tr>';
 		echo '</thead><tbody>';
 
 		while ($row = $data->fetchArray(SQLITE3_ASSOC)) {
 			echo '<tr id="' . $row['timestamp'] . '"><td>' . date("d.m.Y H:i", hexdec(substr($row['timestamp'],0,8))) . '</td>';
 			echo '<td>' . $row['surname'] . ' ' . $row['firstname'] . '</td>';
+			echo '<td>' . $row['year'] . '</td>';
 			echo '<td>';
 
 			foreach(unserialize($row['prescription']) as $prescription) {
