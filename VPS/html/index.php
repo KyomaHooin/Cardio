f8b27676b2016114cd5c5488d1cc409edc52b71c
ブルナ・リチャード <19186867+KyomaHooin@users.noreply.github.com>
2023-07-24 11:40:13 +0200

    Typography.

--- VPS/html/index.php
+++ VPS/html/index.php
@@ -5,10 +5,14 @@ session_start();
 $timestamp = time();
 
 function uuid(){
-        $data = random_bytes(16);
-        $data[6] = chr(ord($data[6]) & 0x0f | 0x40); 
-        $data[8] = chr(ord($data[8]) & 0x3f | 0x80); 
-        return vsprintf('%s%s-%s-%s-%s-%s%s%s', str_split(bin2hex($data), 4));
+	$data = random_bytes(16);
+	$data[6] = chr(ord($data[6]) & 0x0f | 0x40); 
+	$data[8] = chr(ord($data[8]) & 0x3f | 0x80); 
+	return vsprintf('%s%s-%s-%s-%s-%s%s%s', str_split(bin2hex($data), 4));
+}
+
+function typo($text) {
+	return preg_replace('/ ([aiuoksvzAIUOKSVZ]) /', ' $1&nbsp;', $text);
 }
 
 $id = uuid();
@@ -95,18 +99,18 @@ if(!empty($_POST)) {
 <?php
 
 if (!empty($title)) {
-	echo '<div class="p-4 text-center"><h2>' . $title . '</h2></div>';
+	echo '<div class="p-4 text-center"><h2>' . typo($title) . '</h2></div>';
 }
 
 if (!empty($alert)) {
 	echo '<div class="alert alert-warning d-flex align-items-center" role="alert">
 	<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" class="bi bi-exclamation-triangle-fill flex-shrink-0 me-2" viewBox="0 0 16 16" role="img" aria-label="Warning:">
     <path d="M8.982 1.566a1.13 1.13 0 0 0-1.96 0L.165 13.233c-.457.778.091 1.767.98 1.767h13.713c.889 0 1.438-.99.98-1.767L8.982 1.566zM8 5c.535 0 .954.462.9.995l-.35 3.507a.552.552 0 0 1-1.1 0L7.1 5.995A.905.905 0 0 1 8 5zm.002 6a1 1 0 1 1 0 2 1 1 0 0 1 0-2z"/>
-  </svg><div>' . $alert . '</div></div>';
+  </svg><div>' . typo($alert) . '</div></div>';
 }
 
 if (!empty($descr)) {
-	echo '<div class="card"><div class="card-body" style="background-color: #cee5ed;">' . $descr . '</div></div>';
+	echo '<div class="card"><div class="card-body" style="background-color: #cee5ed;">' . typo($descr) . '</div></div>';
 }
 
 ?>
